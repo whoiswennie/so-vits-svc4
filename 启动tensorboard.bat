7e5c388376cd697a32e10ddfbd2a7847e8990096
whoiswennie <3287305464@qq.com>
2025-05-11 13:38:34 +0800

    修复tensorboard

--- 启动tensorboard.bat
+++ 启动tensorboard.bat
@@ -4,6 +4,7 @@ chcp 65001
 echo 正在启动Tensorboard...
 echo 如果看到输出了一条网址（大概率是localhost:6006）就可以访问该网址进入Tensorboard了
 
-tensorboard --logdir=%~dp0logs/44k
+REM 使用指定的 miniconda3 的 python 来运行 tensorboard
+miniconda3\python.exe -m tensorboard.main --logdir=%~dp0logs/44k
 
 pause
